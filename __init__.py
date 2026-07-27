25437506e793fbf0a6a0040f4a443aab0c9acb61
Jáder Vasque <jader.vasque@gmail.com>
2026-07-26 23:43:53 -0400

    fix: install conditional Flux compatibility dispatcher

--- __init__.py
+++ __init__.py
@@ -8,7 +8,13 @@ Model: https://huggingface.co/alibaba-pai/FLUX.2-dev-Fun-Controlnet-Union
 Based on: https://github.com/alibaba/VideoX-Fun
 """
 
+from .flux_patch_compat import apply_compat_patch
+
+# Install before importing nodes. nodes.py still calls flux_patch.apply_patch(),
+# but the shared _patched flag makes that legacy call a no-op.
+apply_compat_patch()
+
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __version__ = "1.1.0"
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
